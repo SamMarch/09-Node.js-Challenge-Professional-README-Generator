3ca5848461687db59f0c68303e7d2f9ac81f3c52
Sammy <marchsam1988@gmail.com>
2022-08-02 16:13:42 +0930

    added most functionality, but the final generate function is not defined and needs fixing

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,65 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please describe your project."
+    },
+    {
+        type: "input",
+        name: "Table of Contents",
+        message: "Please list the contents of your project."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please describe the installation process."
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please describe the usage of your project."
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please describe the license of your project."
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please describe the contributing process."
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+
+    console.log("Successfully wrote to " + fileName);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(function (data) {
+        const readme = generateMarkdown(data);
+        writeToFile("README.md", readme);
+    }
+    );
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,86 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    case "APACHE":
+      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    case "GPL":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    case "BSD":
+      return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+    case "None":
+      return "";
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      return "[MIT License](https://opensource.org/licenses/MIT)";
+    case "APACHE":
+      return "[Apache License 2.0](https://opensource.org/licenses/Apache-2.0)";
+    case "GPL":
+      return "[GNU General Public License v3](https://www.gnu.org/licenses/gpl-3.0)";
+    case "BSD":
+      return "[BSD 3-Clause License](https://opensource.org/licenses/BSD-3-Clause)";
+    case "None":
+      return "";
+    default:
+      return "";
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// TODO: Create a function that returns the license section of README If there is no license, return an empty string
+function renderLicenseSection(license) {
+  switch (license) {
+    case "MIT":
+      return `MIT License (MIT) - [MIT License](  https://opensource.org/licenses/MIT)
+`; case "APACHE":
+      return `Apache License 2.0 (APACHE) - [Apache License 2.0](  https://opensource.org/licenses/Apache-2.0)
+`; case "GPL":
+      return `GNU General Public License v3 (GPL) - [GNU General Public License v3](  https://www.gnu.org/licenses/gpl-3.0)
+`; case "BSD":
+      return `BSD 3-Clause License (BSD) - [BSD 3-Clause License](  https://opensource.org/licenses/BSD-3-Clause)
+`; case "None":
+      return "";
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title} ${renderLicenseBadge(data.license)}
 
+  ${data.description} ${renderLicenseLink(data.license)}
+
+  ## Table of Contents
+  ${data.Table_of_Contents}
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${renderLicenseSection(data.license)}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  ${data.questions}
+  
 `;
 }
 
